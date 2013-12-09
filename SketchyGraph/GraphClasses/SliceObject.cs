17bd8f047361f87d7ef5ccd3365f7c125228132a
Andalon <dg4christ08@gmail.com>
2013-12-08 21:41:08 -0500

    Updated Project to include data labeling and displaying percent and label name
    Signed-off-by: Andalon <dg4christ08@gmail.com>

--- SketchyGraph/GraphClasses/SliceObject.cs
+++ SketchyGraph/GraphClasses/SliceObject.cs
@@ -6,6 +6,7 @@ using System.Windows.Shapes;
 using System.Windows;
 using System.Windows.Media;
 using SketchyGraph.Util;
+using System.Windows.Controls;
 
 namespace SketchyGraph.GraphClasses
 {
@@ -18,6 +19,10 @@ namespace SketchyGraph.GraphClasses
         private Line line2;
         private Path polyPath;
         private List<Line> highlightedLines;
+        public TextBlock dataNameBox;
+        public TextBlock dataValBox;
+        public Point textLocation;
+        public Point dataLocation;
 
         public SliceObject()
         {
@@ -38,6 +43,58 @@ namespace SketchyGraph.GraphClasses
             percentage = 0.0;
             angle = 0.0;
             highlightedLines = new List<Line>();
+            dataNameBox = new TextBlock();
+            dataNameBox.FontSize = 20;
+            dataNameBox.Text = "";
+            dataNameBox.Background = Brushes.White;
+            dataValBox = new TextBlock();
+            dataValBox.FontSize = 20;
+            dataValBox.Text = "";
+            dataValBox.Background = Brushes.White;
+        }
+
+        public Point CalculateTextboxLocation(Circle circle, double factor)
+        {
+            double newX = (line1.X2 + line2.X2) / 2.0;
+            double newY = (line1.Y2 + line2.Y2) / 2.0;
+
+            Vector vecMid;
+            if (angle <= 180)
+            {
+                vecMid = new Vector((-1.0)*(line1.X1 - newX), (-1.0)*(line1.Y1 - newY));
+                vecMid.Normalize();
+            }
+            else
+            {
+                vecMid = new Vector(line1.X1 - newX, line1.Y1 - newY);
+                vecMid.Normalize();
+            }
+
+            //Point temp = new Point(divX * ((1.5 * circle.Radius) + circle.Center.X), divY * ((1.5 * circle.Radius) + circle.Center.Y));
+            Point temp = new Point(((factor * circle.Radius) * vecMid.X) + line1.X1, ((factor * circle.Radius) * vecMid.Y) + line1.Y1);
+
+
+            return temp;
+        }
+
+        public Point CalculateTextboxLocation2(Circle circle, double factor)
+        {
+            if (textLocation.X <= circle.Center.X && textLocation.Y <= circle.Center.Y)
+            {
+                return new Point(textLocation.X - (2*factor), textLocation.Y - (2*factor));
+            }
+            else if (textLocation.X <= circle.Center.X && textLocation.Y > circle.Center.Y)
+            {
+                return new Point(textLocation.X - (2*factor), textLocation.Y + (2*factor));
+            }
+            else if (textLocation.X > circle.Center.X && textLocation.Y <= circle.Center.Y)
+            {
+                return new Point(textLocation.X + factor, textLocation.Y - (2*factor));
+            }
+            else
+            {
+                return new Point(textLocation.X + factor, textLocation.Y + factor);
+            }
         }
 
         public void CreatePath(Circle circle, Point rightPoint)
@@ -93,6 +150,8 @@ namespace SketchyGraph.GraphClasses
 
                 this.highlightedLines.Add(newColor);
             }
+            textLocation = CalculateTextboxLocation(circle, 1.3);
+            dataLocation = CalculateTextboxLocation2(circle, 12);
         }
 
         #region Getter/Setters
